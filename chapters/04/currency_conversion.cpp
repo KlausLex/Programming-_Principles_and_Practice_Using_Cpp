5f93f0fb28d2bd2b6e45e72126d087d3fba1aee0
Klaus Lex <klaus@klauslex.com>
2022-09-27 18:22:24 +0200

    updated currency converter exercise

---
+++ chapters/04/currency_conversion.cpp
@@ -0,0 +1,19 @@
+#include "../../std_facilities.h"
+
+int main(){
+    constexpr double cm_per_inch = 2.54;    // number of centimeters in an inch
+    constexpr double yen_per_krona = 12.74;     // number of yen worth one krona
+    constexpr double pounds_per_krona = 0.082;  // number of pounds worth one krona
+
+    double length = 1;                      // length in inches or centimeters
+    char unit = ' ';                        // a space is not a unit
+
+    cout<< "Please enter an amount of krona followed by a unit to convert (y=yen or p=pounds):\n"; cin >> length >> unit;
+
+    if (unit == 'i')
+        cout << length << "in == " << cm_per_inch*length << "cm\n";
+    else if (unit == 'c')
+        cout << length << "cm == " << length/cm_per_inch << "in\n";
+    else
+        cout << "Sorry, I don't know a unit called '" << unit << "'\n";
+}
