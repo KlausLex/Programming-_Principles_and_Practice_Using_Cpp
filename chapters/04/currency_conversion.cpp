fa190809f78d5494ce695d9f9e8196c6930ec2f3
Klaus Lex <klaus@klauslex.com>
2022-09-29 05:35:13 +0200

    updated currency converter

--- chapters/04/currency_conversion.cpp
+++ chapters/04/currency_conversion.cpp
@@ -1,19 +1,35 @@
 #include "../../std_facilities.h"
 
 int main(){
-    constexpr double cm_per_inch = 2.54;    // number of centimeters in an inch
-    constexpr double yen_per_krona = 12.74;     // number of yen worth one krona
-    constexpr double pounds_per_krona = 0.082;  // number of pounds worth one krona
+    constexpr double krona_per_pound = 12.19;   // amount of swedish krona worth one british pound
+    constexpr double krona_per_yen = 0.078;     // amound of swedish krona worth one japanese yen
+    constexpr double yen_per_krona = 12.74;     // amount of japanese yen worth one swedish krona
+    constexpr double yen_per_pound = 155.81;    // amount of japanese yen worth one british pound
+    constexpr double pounds_per_krona = 0.082;  // amount of british pounds worth one swedish krona
+    constexpr double pounds_per_yen = 0.0064;   // amount of british pounds worth one japanese yen
 
-    double length = 1;                      // length in inches or centimeters
-    char unit = ' ';                        // a space is not a unit
+    double amount = 1;                          // amount in yen, pounds
+    char unit_from = ' ';                       // a space is not a unit
+    char unit_to = ' ';                     
 
-    cout<< "Please enter an amount of krona followed by a unit to convert (y=yen or p=pounds):\n"; cin >> length >> unit;
+    cout<< "Please enter an amount of money followed by its currency to convert from, followed by the currency to convert it to (k = krona, y = yen or p = pounds):\n";
+    cin >> amount >> unit_from >> unit_to;
 
-    if (unit == 'y')
-        cout << length << "in == " << cm_per_inch*length << "yen\n";
-    else if (unit == 'p')
-        cout << length << "cm == " << length/cm_per_inch << "pounds\n";
+    if (unit_from == 'y')
+        if (unit_to == 'k')
+            cout << amount << "yen == " << yen_per_krona * amount << "krona\n";
+        else if (unit_to == 'p')
+        cout << amount << "yen == " << amount / cm_per_inch << "pounds\n";
+    else if (unit_from == 'k')
+        if (unit_to == 'p')
+            cout << amount << "krona == " << yen_per_krona << "pounds\n";
+        else if (unit to == 'y')
+            count << amount << "krona == " << yen_per_krona << "yen\n";
+    else if (unit from == 'p')
+        if (unit_to == 'k')
+            cout << amount << "pounds == " << pounds_per_krona << "krona\n";
+        else if (unit_to == 'y')
+            cout << amount << "pounds == " << yen_per_pounds << "yen\n";
     else
         cout << "Sorry, I don't know a unit called '" << unit << "'\n";
 }
