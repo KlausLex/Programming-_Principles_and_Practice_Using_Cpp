9e1b21eb25482b95d40ef7ac298585e7c9851da4
Klaus Lex <klaus@klauslex.com>
2022-09-29 06:15:06 +0200

    finished currency converter exercise

--- chapters/04/currency_conversion.cpp
+++ chapters/04/currency_conversion.cpp
@@ -11,40 +11,50 @@ int main(){
     double amount = 1;                          // amount in yen, pounds
     char unit_from = ' ';                       // a space is not a unit
     char unit_to = ' ';      
-    string unit = " ";           
 
     cout<< "Please enter an amount of money followed by its currency to convert from, followed by the currency to convert it to (k = krona, y = yen or p = pounds):\n";
     cin >> amount >> unit_from >> unit_to;
-    unit = unit_from + " / " + unit_to;
 
     // from japanese yen..
-    if (unit_from == 'y')
+    if (unit_from == 'y'){
         // ..to swedish krona
-        if (unit_to == 'k')
-            cout << amount << "yen == " << yen_per_krona * amount << "krona\n";
+        if (unit_to == 'k'){
+            cout << amount << " yen == " << yen_per_krona * amount << " krona\n";
+        }
         // ..to british pounds
-        else if (unit_to == 'p')
-            cout << amount << "yen == " << yen_per_pound << "pounds\n";
-
+        else if (unit_to == 'p'){
+            cout << amount << " yen == " << yen_per_pound << " pounds\n";
+        }
+        else
+            cout << "Sorry, I don't know a unit called '" << unit_to << "'\n";
+    }
     // from swedish krona..
-    else if (unit_from == 'k')
+    else if (unit_from == 'k'){
         // ..to british pounds
-        if (unit_to == 'p')
-            cout << amount << "krona == " << krona_per_pound << "pounds\n";
+        if (unit_to == 'p'){
+            cout << amount << " krona == " << krona_per_pound << " pounds\n";
+        }
         // ..to japanese yen
-        else if (unit_to == 'y')
-            cout << amount << "krona == " << krona_per_yen << "yen\n";
-
+        else if (unit_to == 'y'){
+            cout << amount << " krona == " << krona_per_yen << " yen\n";
+        }
+        else
+            cout << "Sorry, I don't know a unit called '" << unit_to << "'\n";
+    }
     // from british pounds..
-    else if (unit_from == 'p')
+    else if (unit_from == 'p'){
         // ..to swedish krona
-        if (unit_to == 'k')
-            cout << amount << "pounds == " << pounds_per_krona << "krona\n";
+        if (unit_to == 'k'){
+            cout << amount << " pounds == " << pounds_per_krona << " krona\n";
+        }
         // ..to japanese yen
-        else if (unit_to == 'y')
-            cout << amount << "pounds == " << yen_per_pound << "yen\n";
-
+        else if (unit_to == 'y'){
+            cout << amount << " pounds == " << yen_per_pound << " yen\n";
+        }
+        else
+            cout << "Sorry, I don't know a unit called '" << unit_to << "'\n";
+    }
     // unknow currency
     else
-        cout << "Sorry, I don't know a unit called '" << unit << "'\n";
+        cout << "Sorry, I don't know a unit called '" << unit_from << "'\n";
 }
