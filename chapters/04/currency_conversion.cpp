3c011cb4a03d18fb1b3da71d97b2118444aed678
Klaus Lex <klaus@klauslex.com>
2022-09-29 05:44:12 +0200

    updated variables in currency converter

--- chapters/04/currency_conversion.cpp
+++ chapters/04/currency_conversion.cpp
@@ -10,26 +10,31 @@ int main(){
 
     double amount = 1;                          // amount in yen, pounds
     char unit_from = ' ';                       // a space is not a unit
-    char unit_to = ' ';                     
+    char unit_to = ' ';      
+    string unit = " ";           
 
     cout<< "Please enter an amount of money followed by its currency to convert from, followed by the currency to convert it to (k = krona, y = yen or p = pounds):\n";
     cin >> amount >> unit_from >> unit_to;
+    unit = unit_from + " / " + unit_to;
 
     if (unit_from == 'y')
         if (unit_to == 'k')
             cout << amount << "yen == " << yen_per_krona * amount << "krona\n";
         else if (unit_to == 'p')
-        cout << amount << "yen == " << amount / cm_per_inch << "pounds\n";
+            cout << amount << "yen == " << yen_per_pound << "pounds\n";
+
     else if (unit_from == 'k')
         if (unit_to == 'p')
-            cout << amount << "krona == " << yen_per_krona << "pounds\n";
-        else if (unit to == 'y')
-            count << amount << "krona == " << yen_per_krona << "yen\n";
-    else if (unit from == 'p')
+            cout << amount << "krona == " << krona_per_pound << "pounds\n";
+        else if (unit_to == 'y')
+            cout << amount << "krona == " << krona_per_yen << "yen\n";
+
+    else if (unit_from == 'p')
         if (unit_to == 'k')
             cout << amount << "pounds == " << pounds_per_krona << "krona\n";
         else if (unit_to == 'y')
-            cout << amount << "pounds == " << yen_per_pounds << "yen\n";
+            cout << amount << "pounds == " << yen_per_pound << "yen\n";
+
     else
         cout << "Sorry, I don't know a unit called '" << unit << "'\n";
 }
